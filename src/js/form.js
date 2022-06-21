5c6f7b788ba562b865914428fbcd3bb04b94d741
Tomás Machado <59934625+tomas-machado@users.noreply.github.com>
2022-06-22 00:14:32 +0100

    feat: submit contact form

--- src/js/form.js
+++ src/js/form.js
@@ -7,11 +7,13 @@ const emailErrorElement = document.getElementById('form-error-email');
 const messageElement = document.getElementById('message');
 const messageErrorElement = document.getElementById('form-error-message');
 
+const formSubmitInput = document.getElementById('form-submit-input');
+
 const formElement = document.getElementById('contact-form');
 
 formElement.addEventListener('submit', (e) => {
-  clearInputErrors();
   e.preventDefault();
+  clearInputErrors();
   checkFormInputs();
 });
 
@@ -19,22 +21,35 @@ function checkFormInputs() {
   const name = nameElement?.value;
   const email = emailElement?.value;
   const message = messageElement?.value;
+  let isError = false;
 
   if (!name) {
     setInputErrorText(nameErrorElement, 'Nome inválido');
+    isError = true;
   }
 
   if (!name || !email.includes('@') || email.length <= 3) {
     setInputErrorText(emailErrorElement, 'Email inválido');
+    isError = true;
   }
 
   if (!message || message.length < 10 || message.length > 500) {
     setInputErrorText(messageErrorElement, 'A mensagem deve ter entre 10 e 500 caracteres.');
+    isError = true;
+  }
+
+  if (!isError) {
+    setInputSubmitText('Obrigado pela sua mensagem!');
   }
 }
 
 function setInputErrorText(errorElement, errorMessage) {
   errorElement.innerText = errorMessage;
+}
+
+function setInputSubmitText(message) {
+  formSubmitInput.value = message;
+  formSubmitInput.disabled = true;
 }
 
 function clearInputErrors() {
